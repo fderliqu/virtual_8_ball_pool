02875c9bd3ad2546e7ee53fa596d94cf052ffc58
fderliqu <florian.derlique@gmail.com>
2022-09-08 15:46:06 +0200

    Add public variables/setters/getters

--- components/Ball.java
+++ components/Ball.java
@@ -2,6 +2,10 @@ public class Ball {
     private double posX;
     private double posY;
 
+    private int ballType;
+    private int ballNumber;
+    private boolean isDropped;
+
     private double speedX;
     private double speedY;
 
@@ -24,4 +28,13 @@ public class Ball {
 
     public double getSpeedY() { return speedY; }
     public void setSpeedY(double speedY) { this.speedY = speedY; }
+
+    public int getBallType() { return ballType; }
+    public void setBallType(int ballType) { this.ballType = ballType; }
+
+    public int getBallNumber() { return ballNumber; }
+    public void setBallNumber(int ballNumber) { this.ballNumber = ballNumber;}
+
+    public boolean getIsDropped() { return isDropped;}
+    public void setIsDropped(boolean isDropped) { this.isDropped = isDropped;}
 }
