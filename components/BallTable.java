d9e2a9e2d9ee057208a2d360bb4af031e96c12cd
fderliqu <florian.derlique@gmail.com>
2022-10-08 20:24:15 +0200

    method update

--- components/BallTable.java
+++ components/BallTable.java
@@ -8,6 +8,7 @@ import java.awt.event.*;
 import java.awt.*;
 
 import components.Ball.BallTypeEnum;
+import libs.Constants;
 
 public class BallTable {
     private ArrayList<Ball> balls = new ArrayList<Ball>();
@@ -19,12 +20,21 @@ public class BallTable {
     private boolean cursorOnDrag = false;
 
     protected Renderer panel;
+
+    double PxPerCm = Constants.POOL_TABLE_LENGTH / Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+
+    private long LastTime;
+    private long NewTime;
+    private long RefTime;
     
     public BallTable(Player player1, Player player2){
         this.player1 = player1;
         this.player2 = player2;
-        balls.add(new Ball(10, 10, 0, 0, BallTypeEnum.WHITE, 0));
-        balls.add(new Ball(30, 10, 0, 0, BallTypeEnum.PLAIN, 1));
+        //double originX = (Constants.POOL_TABLE_LENGTH - Constants.GAME_SURFACE_LENGTH)/2 + Constants.HORIZONTAL_OFFSET_CM;
+        //double originY = (Constants.POOL_TABLE_WIDTH - Constants.GAME_SURFACE_WIDTH)/2 + Constants.VERTICAL_OFFSET_CM;
+        balls.add(new Ball(Constants.WALL_THICKNESS + Constants.START_ZONE,Constants.WALL_THICKNESS + Constants.VERTICAL_OFFSET_CM + Constants.GAME_SURFACE_WIDTH/2, 300, 300, BallTypeEnum.WHITE, 0));
+        //System.out.println("verticalOffset:"+Constants.VERTICAL_OFFSET_CM+" honroOffset:"+Constants.HORIZONTAL_OFFSET_CM);
+        /*balls.add(new Ball(30, 10, 0, 0, BallTypeEnum.PLAIN, 1));
         balls.add(new Ball(50, 10, 0, 0, BallTypeEnum.PLAIN, 2));
         balls.add(new Ball(70, 10, 0, 0, BallTypeEnum.PLAIN, 3));
         balls.add(new Ball(90, 10, 0, 0, BallTypeEnum.PLAIN, 4));
@@ -39,7 +49,9 @@ public class BallTable {
         balls.add(new Ball(270, 10, 0, 0, BallTypeEnum.STRIPED, 13));
         balls.add(new Ball(290, 10, 0, 0, BallTypeEnum.STRIPED, 14));
         balls.add(new Ball(310, 10, 0, 0, BallTypeEnum.STRIPED, 15));
-
+*/      
+        panel = new Renderer(balls);
+        RefTime = System.currentTimeMillis();
         panel.addMouseMotionListener(new MouseMotionAdapter(){
             //Only for tracking display
             public void mouseMoved(MouseEvent event){
@@ -52,18 +64,20 @@ public class BallTable {
 
         panel.addMouseListener(new MouseAdapter(){
             public void mousePressed(MouseEvent event){
-                if(!checkBallsSpeed()){
+                if(checkBallsSpeed()){
                     pressedX = event.getX();
                     pressedY = event.getY();
                 }
+                System.out.println("pressedX : "+pressedX+" draggedY : "+draggedY);
             }
             public void mouseReleased(MouseEvent event){
-                if(!checkBallsSpeed()){
+                if(checkBallsSpeed()){
                 draggedX = event.getX();
                 draggedY = event.getY();
                 }
             }
         });
+        NewTime = System.nanoTime();
     }
 
     public boolean checkBallsSpeed(){
@@ -71,5 +85,25 @@ public class BallTable {
             if(b.getSpeedX() + b.getSpeedY() > 0)return false;
         }
         return true;
+    }
+
+    public void update(){
+        double lastXpos,lastYpos;
+        boolean flagDrawUpdate = false;
+        LastTime = NewTime;
+        NewTime = System.nanoTime();
+        double delta = (NewTime-LastTime)/(1E9);
+        for(Ball b : this.balls){
+            //System.out.println("posX:" + b.getPosX() + " posY:" + b.getPosY()+"speedX:"+b.getSpeedX());
+            lastXpos = b.getPosX();
+            lastYpos = b.getPosY();
+            b.wallCollide();
+            b.update(delta);
+            if((int)(Constants.PX_PER_CM*b.getPosX()) == (int)(lastXpos*Constants.PX_PER_CM) || (int)(Constants.PX_PER_CM*b.getPosY()) == (int)(lastYpos*Constants.PX_PER_CM)){
+                flagDrawUpdate = true;
+            }
+            
+        }
+        if(flagDrawUpdate)panel.drawUpdate();
     }
 }
