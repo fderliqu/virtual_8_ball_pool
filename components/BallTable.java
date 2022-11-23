0980d09ecc315a010e5824aa50789b45d6e18b40
fderliqu <florian.derlique@gmail.com>
2022-11-23 13:43:02 +0100

    New patch for rasengan bug

--- components/BallTable.java
+++ components/BallTable.java
@@ -19,42 +19,42 @@ public class BallTable {
     
     public BallTable(Rules rules){
         this.rules = rules;
-        balls.add(new Ball(WALL_THICKNESS + START_ZONE,WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2, 0, 0, BallTypeEnum.WHITE, 0));
-        balls.add(new Ball(180, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2, 0, 0, BallTypeEnum.PLAIN, 1));
-        balls.add(new Ball(180 + BALL_SIZE, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 + BALL_SIZE/2, 0, 0, BallTypeEnum.PLAIN, 2));
-        balls.add(new Ball(180 + BALL_SIZE, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 - BALL_SIZE/2, 0, 0, BallTypeEnum.PLAIN, 3));
-        balls.add(new Ball(180 + BALL_SIZE*2, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2, 0, 0, BallTypeEnum.PLAIN, 4));
-        balls.add(new Ball(180 + BALL_SIZE*2, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 + BALL_SIZE, 0, 0, BallTypeEnum.PLAIN, 5));
-        balls.add(new Ball(180 + BALL_SIZE*2, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 - BALL_SIZE, 0, 0, BallTypeEnum.PLAIN, 6));
-        balls.add(new Ball(180 + BALL_SIZE*3, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 + BALL_SIZE/2, 0, 0, BallTypeEnum.PLAIN, 7));
-        balls.add(new Ball(180 + BALL_SIZE*3, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 - BALL_SIZE/2, 0, 0, BallTypeEnum.BLACK, 8));
-        balls.add(new Ball(180 + BALL_SIZE*3, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 + BALL_SIZE/2 + BALL_SIZE, 0, 0, BallTypeEnum.STRIPED, 9));
-        balls.add(new Ball(180 + BALL_SIZE*3, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 - BALL_SIZE/2 - BALL_SIZE, 0, 0, BallTypeEnum.STRIPED, 10));
-        balls.add(new Ball(180 + BALL_SIZE*4, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2, 0, 0, BallTypeEnum.STRIPED, 11));
-        balls.add(new Ball(180 + BALL_SIZE*4, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 + BALL_SIZE, 0, 0, BallTypeEnum.STRIPED, 12));
-        balls.add(new Ball(180 + BALL_SIZE*4, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 - BALL_SIZE, 0, 0, BallTypeEnum.STRIPED, 13));
-        balls.add(new Ball(180 + BALL_SIZE*4, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 + 2*BALL_SIZE, 0, 0, BallTypeEnum.STRIPED, 14));
-        balls.add(new Ball(180 + BALL_SIZE*4, WALL_THICKNESS + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH/2 - 2*BALL_SIZE, 0, 0, BallTypeEnum.STRIPED, 15));
+        balls.add(new Ball(BALLS_INIT_POS_X[0],BALLS_INIT_POS_Y[0], 0, 0, BallTypeEnum.WHITE, 0));
+        balls.add(new Ball(BALLS_INIT_POS_X[1],BALLS_INIT_POS_Y[1], 0, 0, BallTypeEnum.PLAIN, 1));
+        balls.add(new Ball(BALLS_INIT_POS_X[2],BALLS_INIT_POS_Y[2], 0, 0, BallTypeEnum.PLAIN, 2));
+        balls.add(new Ball(BALLS_INIT_POS_X[3],BALLS_INIT_POS_Y[3], 0, 0, BallTypeEnum.PLAIN, 3));
+        balls.add(new Ball(BALLS_INIT_POS_X[4],BALLS_INIT_POS_Y[4], 0, 0, BallTypeEnum.PLAIN, 4));
+        balls.add(new Ball(BALLS_INIT_POS_X[5],BALLS_INIT_POS_Y[5], 0, 0, BallTypeEnum.PLAIN, 5));
+        balls.add(new Ball(BALLS_INIT_POS_X[6],BALLS_INIT_POS_Y[6], 0, 0, BallTypeEnum.PLAIN, 6));
+        balls.add(new Ball(BALLS_INIT_POS_X[7],BALLS_INIT_POS_Y[7], 0, 0, BallTypeEnum.PLAIN, 7));
+        balls.add(new Ball(BALLS_INIT_POS_X[8],BALLS_INIT_POS_Y[8], 0, 0, BallTypeEnum.BLACK, 8));
+        balls.add(new Ball(BALLS_INIT_POS_X[9],BALLS_INIT_POS_Y[9], 0, 0, BallTypeEnum.STRIPED, 9));
+        balls.add(new Ball(BALLS_INIT_POS_X[10],BALLS_INIT_POS_Y[10], 0, 0, BallTypeEnum.STRIPED, 10));
+        balls.add(new Ball(BALLS_INIT_POS_X[11],BALLS_INIT_POS_Y[11], 0, 0, BallTypeEnum.STRIPED, 11));
+        balls.add(new Ball(BALLS_INIT_POS_X[12],BALLS_INIT_POS_Y[12], 0, 0, BallTypeEnum.STRIPED, 12));
+        balls.add(new Ball(BALLS_INIT_POS_X[13],BALLS_INIT_POS_Y[13], 0, 0, BallTypeEnum.STRIPED, 13));
+        balls.add(new Ball(BALLS_INIT_POS_X[14],BALLS_INIT_POS_Y[14], 0, 0, BallTypeEnum.STRIPED, 14));
+        balls.add(new Ball(BALLS_INIT_POS_X[15],BALLS_INIT_POS_Y[15], 0, 0, BallTypeEnum.STRIPED, 15));
 
-        holes.add(new RoundHole( WALL_THICKNESS/2 + ANGLE_HOLE_DIAMETER/2 + HORIZONTAL_OFFSET_CM,
-                            WALL_THICKNESS/2 + ANGLE_HOLE_DIAMETER/2 + VERTICAL_OFFSET_CM, 
-                            ANGLE_HOLE_DIAMETER));
-        holes.add(new RoundHole( WALL_THICKNESS/2 + MID_HOLE_DIAMETER/2 + HORIZONTAL_OFFSET_CM + GAME_SURFACE_LENGTH/2,
-                            WALL_THICKNESS/2 + MID_HOLE_DIAMETER/2 + VERTICAL_OFFSET_CM, 
-                            MID_HOLE_DIAMETER));
-        holes.add(new RoundHole( WALL_THICKNESS/2 + ANGLE_HOLE_DIAMETER/2 + HORIZONTAL_OFFSET_CM + GAME_SURFACE_LENGTH,
-                            WALL_THICKNESS/2 + ANGLE_HOLE_DIAMETER/2 + VERTICAL_OFFSET_CM, 
-                            ANGLE_HOLE_DIAMETER));
-        holes.add(new RoundHole( WALL_THICKNESS/2 + ANGLE_HOLE_DIAMETER/2 + HORIZONTAL_OFFSET_CM,
-                            WALL_THICKNESS/2 + ANGLE_HOLE_DIAMETER/2 + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH, 
-                            ANGLE_HOLE_DIAMETER));
-        holes.add(new RoundHole( WALL_THICKNESS/2 + MID_HOLE_DIAMETER/2 + HORIZONTAL_OFFSET_CM + GAME_SURFACE_LENGTH/2,
-                            WALL_THICKNESS/2 + MID_HOLE_DIAMETER/2 + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH, 
-                            MID_HOLE_DIAMETER));
-        holes.add(new RoundHole( WALL_THICKNESS/2 + ANGLE_HOLE_DIAMETER/2 + HORIZONTAL_OFFSET_CM + GAME_SURFACE_LENGTH,
-                            WALL_THICKNESS/2 + ANGLE_HOLE_DIAMETER/2 + VERTICAL_OFFSET_CM + GAME_SURFACE_WIDTH, 
-                            ANGLE_HOLE_DIAMETER));
+        holes.add(new RoundHole(HOLES_INIT_POS_X[0],HOLES_INIT_POS_Y[0],ANGLE_HOLE_DIAMETER));
+        holes.add(new RoundHole(HOLES_INIT_POS_X[1],HOLES_INIT_POS_Y[1],MID_HOLE_DIAMETER));
+        holes.add(new RoundHole(HOLES_INIT_POS_X[2],HOLES_INIT_POS_Y[2],ANGLE_HOLE_DIAMETER));
+        holes.add(new RoundHole(HOLES_INIT_POS_X[3],HOLES_INIT_POS_Y[3],ANGLE_HOLE_DIAMETER));
+        holes.add(new RoundHole(HOLES_INIT_POS_X[4],HOLES_INIT_POS_Y[4],MID_HOLE_DIAMETER));
+        holes.add(new RoundHole(HOLES_INIT_POS_X[5],HOLES_INIT_POS_Y[5],ANGLE_HOLE_DIAMETER));
 
+        NewTime = System.nanoTime();
+    }
+
+    public void resetTable(){
+        int i=0;
+        for(Ball b : balls){
+            b.setPos(BALLS_INIT_POS_X[i],BALLS_INIT_POS_Y[i]);
+            b.setSpeed(0, 0);
+            for(int j=0;j<b.getChecked().length;j++)b.setChecked(false, j);
+            b.setIsDropped(false);
+            i++;
+        }
         NewTime = System.nanoTime();
     }
 
@@ -75,20 +75,25 @@ public class BallTable {
     * updates the positions of every ball on the table
     * */
     public void update(){
+        /*Get time */
         LastTime = NewTime;
         NewTime = System.nanoTime();
         double delta = (NewTime-LastTime)/(1E9);
-        //System.out.println(delta);
+
+        /*Ball checking collision loop */
         for(Ball b : this.balls){
+            /*If ball is already dropped then continue */
             if(b.getIsDropped())continue;
-            //System.out.println("posX:" + b.getPosX() + " posY:" + b.getPosY()+" speedX:"+b.getSpeedX()+" speedY:"+b.getSpeedY());
+
             for(Ball b2 : this.balls){
+                /*if same ball or ball2 is already dropped, skip this loop */
                 if(!b.equals(b2) && !b2.getIsDropped()){
-                    if(b.isColliding(b2) && (b.hasSpeed() || b2.hasSpeed()) && !b.getChecked()[b2.getBallNumber()]){
-                        //System.out.print("c");
-                        //System.out.println(b.getBallNumber() + " speedX:" + b.getSpeedX() + " speedY:" + b.getSpeedY() + " " + b2.getBallNumber()); 
+                    /*if collide and balls has speed and collision is not checked yet then : */
+                    if(b.isColliding(b2) && (b.hasSpeed() || b2.hasSpeed()) && (!b.getChecked()[b2.getBallNumber()] || !b2.getChecked()[b.getBallNumber()])){
                         b.transfert_energy(b2);
+                        b.setChecked(true, b2.getBallNumber());
                         b2.setChecked(true, b.getBallNumber());
+                        /*First ball touched checker for rules */
                         if(b.getBallType() == BallTypeEnum.WHITE){
                             rules.incWhiteBallCollisions();
                             if(rules.getFirstBallTouch() == BallTypeEnum.NULL)rules.setFirstBallTouch(b2.getBallType()); 
@@ -97,8 +102,13 @@ public class BallTable {
                             rules.incWhiteBallCollisions();
                             if(rules.getFirstBallTouch() == BallTypeEnum.NULL)rules.setFirstBallTouch(b.getBallType()); 
                         }
-                        //System.out.println(b.getBallNumber() + " speedX:" + b.getSpeedX() + " speedY:" + b.getSpeedY() + " aftertranfert");
                     }
+                    /*if this collision already check and balls not collide then update flag */
+                    else if(!b.isColliding(b2) && (b.getChecked()[b2.getBallNumber()] || b2.getChecked()[b.getBallNumber()])){
+                        b.setChecked(false, b2.getBallNumber());
+                        b2.setChecked(false, b.getBallNumber());
+                    }
+
                 }
             }
             for(Hole h : holes){
@@ -138,9 +148,11 @@ public class BallTable {
             }
             if(b.wallCollide())rules.incWallCollisions();
             b.update(delta);
+            /*
             for(int i=0;i<b.getChecked().length;i++){
                 b.setChecked(false, i);
             }
+            */
         }
     }
 
